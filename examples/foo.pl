32ab5e7d3dc90a8ffbc5da56ead6957e5997efc1
Pablo Fischer <pablo@pablo.com.mx>
2009-11-01 16:55:03 -0800

    Rename of some files (typo previouslty) and a fix

--- examples/foo.pl
+++ examples/foo.pl
@@ -6,12 +6,12 @@
 
 use strict;
 use warnings;
-use ImportAllFoos;
+use Getopt::Awesome::Examples::ImportAllFoos;
 use Getopt::Awesome qw(:all);
 
 define_option('foo', 'This is foo from main');
 define_option('bar', 'This is bar from main');
 parse_opts();
-ImportAllFoos->test_this_foo;
+Getopt::Awesome::Examples::ImportAllFoos->test_this_foo;
 
 
